504e79fc9e46cb7932e6da11c9bf887b370ddc54
kimhyeonsoo <168629430+hyeonsoosoo@users.noreply.github.com>
2024-11-25 16:57:53 +0900

    Add files via upload

---
+++ ss.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.write("hello world!")
+
+import streamlit as st
+st.header('st.button')
+
+if st.button('say hello'):
+    st.write('Why hello there')
+else: 
+    st.write('goodbye')
+    
